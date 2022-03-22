df50aec7daa17b2f6b4437e5920f1b5c390e9f7c
chacles <88140137@qq.com>
2022-03-22 20:04:27 +0800

    Update image.sh

--- scriptmodules/admin/image.sh
+++ scriptmodules/admin/image.sh
@@ -173,6 +173,7 @@ function _init_chroot_image() {
     cat /etc/hosts
     sudo sed -i /etc/hosts -e "s/^127.0.0.1 localhost$/127.0.0.1 localhost $(hostname)/"
     cat /etc/hosts
+    cat /etc/hostname
 
     # move /etc/ld.so.preload out of the way to avoid warnings
     mv "$chroot/etc/ld.so.preload" "$chroot/etc/ld.so.preload.bak"
